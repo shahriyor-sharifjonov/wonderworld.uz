d9c9ad09a14ea937488aa263fc67d110e62a5e0c
Shahriyor <shahriyordeveloping@gmail.com>
2021-09-14 21:42:15 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -35,3 +35,30 @@ $('.reviews__slider').slick({
   infinite: false,
 });
 
+$(document).ready(function() {
+  $(".set > button").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".content")
+        .slideUp(200);
+      $(".set > button i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+    } else {
+      $(".set > button i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+      $(this)
+        .find("i")
+        .removeClass("fa-plus")
+        .addClass("fa-minus");
+      $(".set > button").removeClass("active");
+      $(this).addClass("active");
+      $(".content").slideUp(200);
+      $(this)
+        .siblings(".content")
+        .slideDown(200);
+    }
+  });
+});
